1b067646d9276bebbe538f7a28667621208ee780
Phill Watson <watson.phill@gmail.com>
2023-02-10 17:51:42 +0000

    get toolbar visual working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,54 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
 import ToolBar from "./components/tool-bar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/about";
 import Institutions from "./pages/institutions";
 import SignIn from "./pages/sign-in";
 
+function HeaderLayout() {
+  return (
+    <>
+      <header>
+        <ToolBar />
+      </header>
+      <Outlet />
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HeaderLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "institutions",
+        element: <Institutions />
+      },
+      {
+        path: "sign-in",
+        element: <SignIn />
+      },
+    ],
+  },
+]);
+
 export default function App() {
   return (
-    <Router>
-      <ToolBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/institutions" element={<Institutions />} />
-        <Route path="/sign-in" element={<SignIn />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
--- client/src/pages/sign-in.tsx
+++ client/src/pages/sign-in.tsx
@@ -2,14 +2,7 @@ import React from "react";
 
 export default function SignIn() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "Right",
-        alignItems: "Right",
-        height: "100vh"
-      }}
-    >
+    <div>
       <h1>Sign-In</h1>
     </div>
   );
