0b869ef870a348f287ab967be0c5712c3b4e9b27
Phill Watson <watson.phill@gmail.com>
2023-03-13 12:56:42 +0000

    cache account details

---
+++ commons/src/main/java/com/hillayes/commons/net/Network.java
@@ -0,0 +1,21 @@
+package com.hillayes.commons.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Network {
+    /**
+     * Returns the external IP of the calling service host.
+     *
+     * @return the calling service's external IP address.
+     * @throws IOException if the IP address cannot be obtained.
+     */
+    public static String getMyIpAddress() throws IOException {
+        URL url = new URL("http://checkip.amazonaws.com/");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            return br.readLine();
+        }
+    }
+}
