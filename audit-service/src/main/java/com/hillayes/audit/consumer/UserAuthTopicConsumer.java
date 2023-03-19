20bc27d99a9cdfc90dc0bbfa7a19d4275b326910
Phill Watson <watson.phill@gmail.com>
2023-03-19 15:37:19 +0000

    investigate dead-letter message handling

--- audit-service/src/main/java/com/hillayes/audit/consumer/UserAuthTopicConsumer.java
+++ audit-service/src/main/java/com/hillayes/audit/consumer/UserAuthTopicConsumer.java
@@ -20,6 +20,10 @@ public class UserAuthTopicConsumer {
             log.info("Received user_auth event [payloadClass: {}]", payloadClass);
 
             if (LoginFailure.class.getName().equals(payloadClass)) {
+//                log.debug("Event retried: {}", eventPacket.getRetryCount());
+//                if (eventPacket.getRetryCount() == 0) {
+//                    throw new RuntimeException("Simulating event failure ");
+//                }
                 processLoginFailure(eventPacket.getPayloadContent());
             }
 
