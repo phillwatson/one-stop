3b14b14d3826d3e6387ecae945dbbef909b76861
Phill Watson <watson.phill@gmail.com>
2023-03-23 18:50:43 +0000

    Implement google-id login

---
+++ client/src/components/oauth/google-id/google-signin-button.tsx
@@ -0,0 +1,28 @@
+import './google-signin-button.css'; // import stylesheet for styling
+
+interface Props {
+  clientId: string;
+  redirectUri: string;
+
+}
+
+export default function GoogleSignInButton(props: Props) {
+  function buttonClick() {
+    var uri = 'https://accounts.google.com/o/oauth2/auth' +
+    '?response_type=code' +
+    '&state=google' +
+    '&client_id=' + props.clientId +
+    '&scope=openid%20email' +
+    '&redirect_uri=' + encodeURIComponent(props.redirectUri);
+    console.log(uri);
+
+    window.location.href = uri;
+  }
+
+  return (
+    <button className="google-sign-in-button" onClick= { buttonClick }>
+      <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo" />
+      <span>Sign in with Google</span>
+    </button>
+  );
+};
