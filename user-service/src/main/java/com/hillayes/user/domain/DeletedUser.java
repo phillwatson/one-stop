0b61453c5f190ec6057efb78abe1c030b4678c70
Phill Watson <watson.phill@gmail.com>
2023-04-16 11:42:28 +0100

    Move deleted users to another table

--- user-service/src/main/java/com/hillayes/user/domain/DeletedUser.java
+++ user-service/src/main/java/com/hillayes/user/domain/DeletedUser.java
@@ -2,7 +2,6 @@ package com.hillayes.user.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -31,35 +30,27 @@ public class DeletedUser {
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
-    @Setter
     @Column(nullable = false)
     private String email;
 
-    @Setter
     @Column(name = "title", nullable = true)
     private String title;
 
-    @Setter
     @Column(name = "given_name", nullable = false)
     private String givenName;
 
-    @Setter
     @Column(name = "family_name", nullable = true)
     private String familyName;
 
-    @Setter
     @Column(name = "preferred_name", nullable = true)
     private String preferredName;
 
-    @Setter
     @Column(name = "phone_number", nullable = true)
     private String phoneNumber;
 
-    @Builder.Default
     @Column(name = "date_created", nullable = false)
-    private Instant dateCreated = Instant.now();
+    private Instant dateCreated;
 
-    @Setter
     @Column(name = "date_onboarded")
     private Instant dateOnboarded;
 
@@ -69,11 +60,9 @@ public class DeletedUser {
         return dateOnboarded != null;
     }
 
-    @Setter
     @Column(name = "date_deleted")
     private Instant dateDeleted;
 
-    @Setter
     @Column(name = "date_blocked")
     private Instant dateBlocked;
 
@@ -83,7 +72,6 @@ public class DeletedUser {
         return dateBlocked != null;
     }
 
-    @Setter
     @Column(name = "blocked_reason")
     private Instant blockedReason;
 
@@ -94,11 +82,6 @@ public class DeletedUser {
     @JsonIgnore
     public String getPasswordHash() {
         return passwordHash;
-    }
-
-    @JsonProperty
-    protected void setPasswordHash(String aValue) {
-        passwordHash = aValue;
     }
 
     public static DeletedUser fromUser(User user) {
