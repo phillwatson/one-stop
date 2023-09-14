48026b7a2facb62e5aa996d9708a8cf779a79f37
Phill Watson <watson.phill@gmail.com>
2023-09-14 16:40:04 +0100

    Notification service integration

---
+++ integration-tests/src/main/java/com/hillayes/integration/api/NotificationApi.java
@@ -0,0 +1,43 @@
+package com.hillayes.integration.api;
+
+import com.hillayes.onestop.api.*;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.restassured.http.ContentType.JSON;
+
+public class NotificationApi extends ApiBase {
+    private static final TypeRef<List<NotificationResponse>> NOTIFICATION_LIST = new TypeRef<>() {};
+
+    public NotificationApi(Map<String, String> authCookies) {
+        super(authCookies);
+    }
+
+    public List<NotificationResponse> getNotifications(Instant after) {
+        return givenAuth()
+            .queryParam("after", after.toString())
+            .get("/api/v1/notifications")
+            .then()
+            .statusCode(200)
+            .contentType(JSON)
+            .extract().as(NOTIFICATION_LIST);
+    }
+
+    public void deleteNotification(UUID notificationId) {
+        deleteNotification(notificationId, 204);
+    }
+
+    public Response deleteNotification(UUID notificationId, int expectedStatus) {
+        return givenAuth()
+            .contentType(JSON)
+            .delete("/api/v1/notifications/{id}", notificationId)
+            .then()
+            .statusCode(expectedStatus)
+            .extract().response();
+    }
+}
