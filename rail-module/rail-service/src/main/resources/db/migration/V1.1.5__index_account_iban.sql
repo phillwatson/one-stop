f9a615f7a22f93b6b65f10d62c8ad9e40c1347a2
Phill Watson <watson.phill@gmail.com>
2025-09-21 11:23:24 +0100

    Augment find account by IBAN with user ID

--- rail-module/rail-service/src/main/resources/db/migration/V1.1.5__index_account_iban.sql
+++ rail-module/rail-service/src/main/resources/db/migration/V1.1.5__index_account_iban.sql
@@ -1,1 +1,3 @@
-CREATE INDEX idx_account_iban ON rails.account (iban);
+-- replace user-id index with one that includes the IBAN
+DROP INDEX idx_account_user_id CASCADE;
+CREATE INDEX idx_account_iban ON ${flyway:defaultSchema}.account (user_id, iban);
