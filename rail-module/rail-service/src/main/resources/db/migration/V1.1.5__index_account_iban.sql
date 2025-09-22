2eaf91bf350f8f9ee39f9d0c1f81367245ba22d3
Phill Watson <watson.phill@gmail.com>
2025-09-22 07:42:49 +0100

    Correct db migration to comply with modulith schema

--- rail-module/rail-service/src/main/resources/db/migration/V1.1.5__index_account_iban.sql
+++ rail-module/rail-service/src/main/resources/db/migration/V1.1.5__index_account_iban.sql
@@ -1,3 +1,3 @@
 -- replace user-id index with one that includes the IBAN
-DROP INDEX idx_account_user_id CASCADE;
-CREATE INDEX idx_account_iban ON ${flyway:defaultSchema}.account (user_id, iban);
+DROP INDEX rails.idx_account_user_id CASCADE;
+CREATE INDEX idx_account_iban ON rails.account (user_id, iban);
