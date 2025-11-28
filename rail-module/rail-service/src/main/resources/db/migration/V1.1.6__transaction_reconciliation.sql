19c8e8076dd767f63721f46b5efea5061558e5ac
Phill Watson <watson.phill@gmail.com>
2025-11-28 15:09:32 +0000

    Refactor rail db migration for modulith

--- rail-module/rail-service/src/main/resources/db/migration/V1.1.6__transaction_reconciliation.sql
+++ rail-module/rail-service/src/main/resources/db/migration/V1.1.6__transaction_reconciliation.sql
@@ -1,4 +1,4 @@
 -- add reconciled indicator and user notes
-ALTER TABLE ${flyway:defaultSchema}.account_transaction
+ALTER TABLE rails.account_transaction
     ADD COLUMN reconciled boolean NOT NULL DEFAULT FALSE,
     ADD COLUMN notes text NULL;
