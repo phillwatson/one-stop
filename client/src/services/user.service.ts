2f5bd28c56e1667bbd67583b2df2f86f52ec45aa
Phill Watson <watson.phill@gmail.com>
2025-12-01 10:48:28 +0000

    Migrate client pom build to pnpm, and correct open-id redirection

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -12,9 +12,7 @@ export interface RegistrationCredentials {
 class UserService {
   login(provider: string) {
     return http.get<Location>(`auth/login/${provider}`, { params: { "state": "same-state-value" }})
-      .then(response => window.location.assign(response.data.href));
-
-    //window.location.href = window.location.origin + "/api/v1/auth/login/" + provider + "?state=same-state-value";
+      .then(response => window.location.assign(response.data.toString()));
   }
 
   registerNewUser(email: string): Promise<void> {
