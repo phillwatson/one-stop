f1bf9965906d237f838201f979695f7dc8cd6a29
Phill Watson <watson.phill@gmail.com>
2026-01-21 15:06:48 +0000

    Remove redundant code

--- client/src/pages/trans-audit-results-list.tsx
+++ client/src/pages/trans-audit-results-list.tsx
@@ -1,11 +1,7 @@
-import { useNavigate } from "react-router-dom";
-
 import PageHeader from "../components/page-header/page-header";
 import AuditIssuesList from "../components/audit-report/audit-config-list";
 
 export default function TransactionAuditReports() {
-  const navigate = useNavigate();
-
   return (
     <PageHeader title="Transaction Audit Results">
       <AuditIssuesList />
