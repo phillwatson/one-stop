e19dbdf9b7a166b7aef63507a3699f7b70814152
Phill Watson <watson.phill@gmail.com>
2026-02-03 11:47:57 +0000

    Temporarily remove portfolio detail display

--- client/src/pages/share-portfolio-detail.tsx
+++ client/src/pages/share-portfolio-detail.tsx
@@ -1,41 +1,36 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import PortfoliosIcon from '@mui/icons-material/AssessmentOutlined';
 
-import { useMessageDispatch } from '../contexts/messages/context';
 import PageHeader from '../components/page-header/page-header';
-import PortfolioService from '../services/portfolio.service';
-import { PortfolioResponse } from '../model/share-portfolio.model';
 
 export default function SharePortfolioDetail() {
-  const { portfolioId } = useParams();
-  const navigate = useNavigate();
+  // const { portfolioId } = useParams();
+  // const navigate = useNavigate();
 
-  const showMessage = useMessageDispatch();
-  const [ portfolio, setPortfolio ] = useState<PortfolioResponse|undefined>();
+  // const showMessage = useMessageDispatch();
+  // const [ portfolio, setPortfolio ] = useState<PortfolioResponse|undefined>();
 
-  useEffect(() => {
-    if (portfolioId) {
-      PortfolioService.getPortfolio(portfolioId)
-        .then(response => setPortfolio(response))
-        .catch(err => showMessage(err));
-    }
-  }, [ showMessage, portfolioId ]);
+  // useEffect(() => {
+  //   if (portfolioId) {
+  //     PortfolioService.getPortfolio(portfolioId)
+  //       .then(response => setPortfolio(response))
+  //       .catch(err => showMessage(err));
+  //   }
+  // }, [ showMessage, portfolioId ]);
 
-  function close() {
-    navigate('/shares/portfolios');
-  }
+  // function close() {
+  //   navigate('/shares/portfolios');
+  // }
 
-  function handleSubmit(portfolio: PortfolioResponse) {
+  // function handleSubmit(portfolio: PortfolioResponse) {
     // PortfolioService.updatePortfolio(portfolio)
     //   .then(() => close())
     //   .catch(err => showMessage(err));
     // }
-  }
+  // }
 
-  function handleCancel() {
-    close();
-  }
+  // function handleCancel() {
+  //   close();
+  // }
 
   return (
     <PageHeader title='Share Portfolio' icon={ <PortfoliosIcon /> }>
